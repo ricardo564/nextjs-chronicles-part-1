c1fcc66a6a28c3e03a0a365f55952b2228b0304a
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 20:56:09 -0300

    Add TopSellingSection component to enhance Home page layout

---
+++ src/blocks/sections/topSellingSection.tsx
@@ -0,0 +1,9 @@
+export default async function TopSellingSection() {
+    return (
+      <div
+        className="relative min-h-screen max-w-7xl mx-auto"
+      >
+        <h1>Top Selling Section</h1>
+      </div>
+    )
+}
