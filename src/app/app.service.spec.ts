ba7e936b875cee3bf7dd55fc445751b2f07d72d6
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 14:11:26 -0300

    Add unit tests for AppService to ensure service is defined

---
+++ src/app/app.service.spec.ts
@@ -0,0 +1,18 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { AppService } from './app.service';
+
+describe('AppService', () => {
+  let service: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [AppService],
+    }).compile();
+
+    service = app.get<AppService>(AppService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
