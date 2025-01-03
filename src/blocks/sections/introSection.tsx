1d7344233deffb1e07aa676009c81a19269ea9ba
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 20:56:03 -0300

    Enhance IntroSection styling with background image and layout adjustments. Updated the component to include a full-width background image and improved layout properties for better visual presentation.

--- src/blocks/sections/introSection.tsx
+++ src/blocks/sections/introSection.tsx
@@ -1,6 +1,16 @@
+import background from "@/assets/images/topiary-green-pot.webp";
+
 export default async function IntroSection() {
     return (
-      <div>
+      <div
+        className="relative min-h-[150rem] w-screen max-w-7xl mx-auto"
+        style={{
+          backgroundImage: `url(${background.src})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
         <h1>Intro Section</h1>
       </div>
     )
