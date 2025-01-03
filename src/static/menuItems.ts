41b6c3048b1944982473fd27ac91a48069f2c9ff
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 17:23:48 -0300

    Add menuItems file to define application navigation structure. Introduced a new menuItems.ts file that exports an array of menu items, enhancing the navigation experience by providing clear links to key sections of the application.

---
+++ src/static/menuItems.ts
@@ -0,0 +1,8 @@
+import { MenuItem } from "@/types/menuItem";
+
+export const menuItems: MenuItem[] = [
+  { label: "Home", to: "/", ariaLabel: "Home" },
+  { label: "Plants Types", to: "/plants-types", ariaLabel: "Plants Types" },
+  { label: "More", to: "/more", ariaLabel: "More" },
+  { label: "Contact", to: "/contact", ariaLabel: "Contact" },
+];
