ba57b2a60494263ce22e47148216d6a0bb84af62
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 20:55:58 -0300

    Add TopSellingSection to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import IntroSection from "@/blocks/sections/introSection";
+import TopSellingSection from "@/blocks/sections/topSellingSection";
 import DefaultLayout from "@/layouts/DefaultLayout";
 
 export default async function Home() {
   return (
     <DefaultLayout>
       <IntroSection />
+      <TopSellingSection />
     </DefaultLayout>
   );
 }
