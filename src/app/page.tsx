d0580c6c7f7b6bde0a6e82a5543a4686e2bb3ec9
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 15:22:33 -0300

    Refactor Home component to utilize PlantService for fetching plant data, enhancing error handling and modularity. This update improves the structure of the component by separating API logic and provides clearer user feedback in case of errors during data retrieval.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,40 @@
-import { Plant, PlantApiResponse } from "@/types/plant.types";
 import { PlantCard } from "@/components/PlantCard";
-
-async function getPlants(): Promise<PlantApiResponse> {
-  const apiKey = process.env.TREFLE_API_KEY;
-  const errorMessage = "Failed to fetch plants";
-
-  try {
-    const response = await fetch(
-      `https://trefle.io/api/v1/plants?token=${apiKey}`,
-      {
-        next: {
-          revalidate: 10800,
-        },
-      }
-    );
-
-    if (!response.ok) {
-      throw new Error(errorMessage);
-    }
-
-    return response.json();
-  } catch (error) {
-    console.error(errorMessage, error);
-    throw new Error(errorMessage);
-  }
-}
+import { PlantService } from "@/services/plant.service";
+import { HttpClient } from "@/services/http-client";
 
 export default async function Home() {
-  const { data: plants } = await getPlants();
+  const httpClient = new HttpClient();
+  const plantService = new PlantService(httpClient);
 
-  return (
-    <div className="min-h-screen bg-gray-900 p-8">
-      <div className="flex flex-wrap justify-center items-center gap-6">
-        {!plants.length && (
-          <p className="text-white text-2xl font-bold">
-            Nenhuma planta para ser exibida...
-          </p>
-        )}
+  try {
+    const { data } = await plantService.getPlants();
+    const plants = data;
 
-        {plants.map((plant: Plant) => (
-          <PlantCard key={plant.id} plant={plant} />
-        ))}
+    console.log("plants", plants)
+
+    return (
+      <div className="min-h-screen bg-gray-900 p-8">
+        <div className="flex flex-wrap justify-center items-center gap-6">
+          {!plants?.length && (
+            <p className="text-white text-2xl font-bold">
+              Nenhuma planta para ser exibida...
+            </p>
+          )}
+
+          {plants.map((plant) => (
+            <PlantCard key={plant.id} plant={plant} />
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } catch (error) {
+    console.error('Error fetching plants:', error);
+    return (
+      <div className="min-h-screen bg-gray-900 p-8 flex justify-center items-center">
+        <p className="text-white text-2xl font-bold">
+          Erro ao carregar as plantas...
+        </p>
+      </div>
+    );
+  }
 }
