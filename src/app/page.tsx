c4bac5130addb3e47752cad2b73b3e98aada6982
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 16:28:56 -0300

    Refactor Home component to improve plant data handling and user feedback. Updated plant fetching logic to directly assign typed plants, enhancing type safety. Added conditional rendering for empty plant lists, providing clearer messaging when no plants are available. This update enhances user experience and code clarity.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,29 @@
 import { PlantCard } from "@/components/PlantCard";
 import { PlantService } from "@/services/plant.service";
 import { HttpClient } from "@/services/http-client";
-
+import { Plant } from "@/types/plant.types";
 export default async function Home() {
   const httpClient = new HttpClient();
   const plantService = new PlantService(httpClient);
 
   try {
-    const { data } = await plantService.getPlants();
-    const plants = data;
+    const plants = await plantService.getPlants();
+    const typedPlants = plants as unknown as Plant[];
 
-    console.log("plants", plants)
+    if (typedPlants.length === 0) {
+      return (
+        <div className="min-h-screen bg-gray-900 p-8 flex justify-center items-center">
+          <p className="text-white text-2xl font-bold">
+            Nenhuma planta para ser exibida...
+          </p>
+        </div>
+      );
+    }
 
     return (
       <div className="min-h-screen bg-gray-900 p-8">
         <div className="flex flex-wrap justify-center items-center gap-6">
-          {!plants?.length && (
-            <p className="text-white text-2xl font-bold">
-              Nenhuma planta para ser exibida...
-            </p>
-          )}
-
-          {plants.map((plant) => (
+          {typedPlants.map((plant: Plant) => (
             <PlantCard key={plant.id} plant={plant} />
           ))}
         </div>
