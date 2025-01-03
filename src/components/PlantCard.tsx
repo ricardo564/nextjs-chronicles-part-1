b7e89e650ebf7396efa8b61afabfe0e45122b3a0
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 15:58:05 -0300

    Enhance PlantCard component to display plant family and genus information, and implement random synonym selection for common name. This update improves the component's functionality and user experience by providing more detailed plant information and a dynamic display of names.

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -7,11 +7,49 @@ interface PlantCardProps {
   titleClassName?: string;
 }
 
+// {
+//   id: 262017,
+//   common_name: 'Meadowsweet',
+//   slug: 'filipendula-ulmaria',
+//   scientific_name: 'Filipendula ulmaria',
+//   year: 1879,
+//   bibliography: 'Trudy Imp. S.-Peterburgsk. Bot. Sada 6: 251 (1879)',
+//   author: '(L.) Maxim.',
+//   status: 'accepted',
+//   rank: 'species',
+//   family_common_name: 'Rose family',
+//   genus_id: 12148,
+//   image_url: 'https://bs.plantnet.org/image/o/53c73903dc455a3d734b193dad7d9d8c4ec0e324',
+//   synonyms: [
+//     'Ulmaria ulmaria',
+//     'Thecanisia ulmaria',
+//     'Spiraea ulmaria',
+//     'Spiraea ulmaria f. tomentosa',
+//     'Spiraea ulmaria var. tomentosa'
+//   ],
+//   genus: 'Filipendula',
+//   family: 'Rosaceae',
+//   links: {
+//     self: '/api/v1/species/filipendula-ulmaria',
+//     plant: '/api/v1/plants/filipendula-ulmaria',
+//     genus: '/api/v1/genus/filipendula'
+//   }
+// }
+
 export function PlantCard({
   plant,
   imageClassName,
   titleClassName,
 }: PlantCardProps) {
+
+  const getRandomName = () => {
+    const synonymsLength = plant.synonyms.length;
+
+    const randomIndex = Math.floor(Math.random() * synonymsLength);
+
+    return plant.synonyms[randomIndex];
+  }
+
   return (
     <div
       className="relative bg-black/10 backdrop-blur-xl rounded-[32px] p-8 border border-white/20 w-full max-w-[20rem] flex flex-col items-center"
@@ -30,14 +68,22 @@ export function PlantCard({
         )}
       </div>
 
-      <div className="flex flex-col w-full">
+      <div className="flex flex-col w-full gap-2">
         <p className="text-gray-300 text-sm mb-2">
-          {plant.common_name || plant.scientific_name}
+          {getRandomName()}
         </p>
 
-        <h2 className={`text-white text-2xl font-semibold mb-6 ${titleClassName}`}>
+        <p className="text-gray-300 text-sm">
+            Family: {plant.family}
+        </p>
+
+        <p className="text-gray-300 text-sm">
+          Genus: {plant.genus}
+        </p>
+
+        <p className={`text-white text-2xl font-semibold mb-6 ${titleClassName}`}>
           {plant.bibliography + " " + plant.scientific_name}
-        </h2>
+        </p>
       </div>
 
         <div className="w-full flex items-center justify-start">
