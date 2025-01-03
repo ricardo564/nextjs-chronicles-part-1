6b45efc76385b26c8d6aab7a5ccacf9f85baf959
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 14:52:06 -0300

    Add Next.js configuration for image domains in next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'd2seqvvyy3b8p2.cloudfront.net',
+      'bs.plantnet.org'
+    ],
+  },
+}
+
+module.exports = nextConfig
