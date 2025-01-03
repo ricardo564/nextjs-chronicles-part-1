822ecc16731b057864f5e75fffc0369fb5f03157
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 15:23:46 -0300

    Refactor Next.js configuration by removing next.config.js and consolidating image domain settings into next.config.ts. This change streamlines configuration management and ensures consistency in image handling across the application.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      'd2seqvvyy3b8p2.cloudfront.net',
+      'bs.plantnet.org'
+    ],
+  },
 };
 
 export default nextConfig;
