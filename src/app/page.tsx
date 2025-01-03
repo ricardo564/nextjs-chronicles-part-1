5a2d5104f1b31d4b7d8f400ab1f86304efa384e8
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 20:12:39 -0300

    Refactor Home component to simplify rendering logic. Removed plant fetching and display logic, replacing it with an IntroSection for a cleaner layout. This change enhances maintainability and focuses on the introductory content of the application.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,11 @@
-import { PlantCard } from "@/components/PlantCard";
-import { PlantService } from "@/services/plant.service";
-import { HttpClient } from "@/services/http-client";
-import { Plant } from "@/types/plant.types";
-import { NoDataToShow } from "@/components/NoDataToShow";
+import IntroSection from "@/blocks/sections/introSection";
 import DefaultLayout from "@/layouts/DefaultLayout";
 
 export default async function Home() {
-  const httpClient = new HttpClient();
-  const plantService = new PlantService(httpClient);
-
-  try {
-    const plants = await plantService.getPlants();
-    const typedPlants = plants as unknown as Plant[];
-
-    if (typedPlants.length === 0) {
-      return (
-        <DefaultLayout>
-          <NoDataToShow message="Nenhuma planta para ser exibida..." />
-        </DefaultLayout>
-      );
-    }
-
-    return (
-      <DefaultLayout>
-        <div className="min-h-screen bg-primary p-8">
-          <div className="flex flex-wrap justify-center items-center gap-6">
-            {typedPlants.map((plant: Plant) => (
-              <PlantCard key={plant.id} plant={plant} />
-            ))}
-          </div>
-        </div>
-      </DefaultLayout>
-    );
-  } catch (error) {
-    console.error('Error fetching plants:', error);
-    return (
-      <DefaultLayout>
-        <NoDataToShow message="Erro ao carregar as plantas..." />
-      </DefaultLayout>
-    );
-  }
+  return (
+    <DefaultLayout>
+      <IntroSection />
+    </DefaultLayout>
+  );
 }
+
