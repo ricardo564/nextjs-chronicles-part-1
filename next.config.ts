3a415d7c4d2935f37f0adcbbdd5e3647e3395b35
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 19:53:29 -0300

    Update Next.js configuration for standalone output and unoptimized images. This change enhances deployment flexibility and optimizes image handling for improved performance.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   images: {
+    unoptimized: true,
     domains: [
       'd2seqvvyy3b8p2.cloudfront.net',
       'bs.plantnet.org'
