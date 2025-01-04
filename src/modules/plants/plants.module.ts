5d6491adcaf2fec078833f1b270281127a581ec1
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 09:23:21 -0300

    Refactor PlantsModule: Removed ConfigModule import to simplify module dependencies and enhance modularity. This change streamlines the PlantsModule by focusing on essential imports, improving overall application structure.

--- src/modules/plants/plants.module.ts
+++ src/modules/plants/plants.module.ts
@@ -1,17 +1,12 @@
 import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
-import { ConfigModule } from '@nestjs/config';
 import { PlantsController } from '@/modules/plants/plants.controller';
 import { PlantsService } from '@/modules/plants/plants.service';
 
 @Module({
-  imports: [
-    HttpModule,
-    ConfigModule,
-  ],
+  imports: [HttpModule],
   controllers: [PlantsController],
   providers: [PlantsService],
-  exports: [PlantsService],
 })
 
 export class PlantsModule {}
