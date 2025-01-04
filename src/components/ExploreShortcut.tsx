ee5aa0c56d07cd11d4c57b6901de1d63a68097f9
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-03 22:42:14 -0300

    Add ExploreShortcut component: Introduced a new ExploreShortcut component for easy navigation to plant details. The component features a styled link that enhances user interaction by providing a clear call to action to explore specific plants.

---
+++ src/components/ExploreShortcut.tsx
@@ -0,0 +1,18 @@
+import { Plant } from "@/types/plant.types";
+import Link from "next/link";
+
+interface ExploreShortcutProps {
+  plant: Plant;
+}
+
+export const ExploreShortcut = ({ plant }: ExploreShortcutProps) => {
+  return (
+    <Link
+      className="px-6 py-2 border border-white/20 text-white rounded-lg min-w-[13rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300"
+      title={`Explore ${plant.common_name || plant.scientific_name} plants`}
+      href={`/plants/${plant.id}`}
+    >
+      Explore
+    </Link>
+  );
+};
