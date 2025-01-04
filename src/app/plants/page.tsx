fd5d36eb2ceb2cf96e0816e35ffdd0c0462d1e4b
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 08:53:01 -0300

    Refactor PlantsPage to use fetch API: Replaced PlantService with direct fetch call to retrieve plant data from the API. This change simplifies the data fetching process and improves performance by eliminating unnecessary service layers.

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -1,26 +1,22 @@
 import { PlantCard } from "@/components/PlantCard";
-import { PlantService } from "@/services/plant.service";
-import { HttpClient } from "@/services/http-client";
 import { Plant } from "@/types/plant.types";
 import { NoDataToShow } from "@/components/NoDataToShow";
 import DefaultLayout from "@/layouts/DefaultLayout";
 
 export default async function PlantsPage() {
-  const httpClient = new HttpClient();
-  const plantService = new PlantService(httpClient);
-
   try {
-    const plants = await plantService.getPlants();
-    const typedPlants = plants as unknown as Plant[];
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/plants`);
+    const data = await response.json();
+    const plants: Plant[] = data.data;
 
     return (
       <DefaultLayout>
         <div className="py-8">
-          {typedPlants.length === 0 ? (
+          {plants.length === 0 ? (
             <NoDataToShow message="Nenhuma planta para ser exibida..." />
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 gap-y-[10rem] pt-[8rem]">
-              {typedPlants.map((plant: Plant) => (
+              {plants.map((plant) => (
                 <PlantCard key={plant.id} plant={plant} />
               ))}
             </div>
