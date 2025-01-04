0cb3c554f63be199ba77c3f8e109195cf24cb075
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 09:23:06 -0300

    Update AppModule: Change environment file path to '.env.local' and remove unused AppController and AppService imports to streamline the application structure and improve modularity.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppService } from '@/app/app.service';
-import { AppController } from '@/app/app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { HttpModule } from '@nestjs/axios';
 import { PlantsModule } from '@/modules/plants/plants.module';
@@ -9,13 +7,11 @@ import { PlantsModule } from '@/modules/plants/plants.module';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env'
+      envFilePath: '.env.local',
     }),
     HttpModule,
     PlantsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 
 export class AppModule {}
