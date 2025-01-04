906c6d4b703933fd35aba729b147cf9e768725b1
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 08:09:25 -0300

    Add mockup plant data: Introduced a new plant entry for Dieffenbachia amoena in the mockupPlants dataset, including detailed attributes such as common name, scientific name, synonyms, and API links. This enhances the plant collection representation for development and testing purposes.

--- src/static/mockupPlants.ts
+++ src/static/mockupPlants.ts
@@ -28,4 +28,30 @@ export const mockupPlants: Plant[] = [
       genus: "/api/v1/genus/dieffenbachia"
     },
   },
+  {
+    id: 2,
+    common_name: "Dieffenbachia",
+    slug: "dieffenbachia-amoena",
+    scientific_name: "Dieffenbachia amoena",
+    year: 1977,
+    bibliography: "Exotic Foliage Plants",
+    author: "Heinrich Wilhelm Schott",
+    status: "accepted",
+    rank: "species",
+    family_common_name: "Araceae",
+    genus_id: 3275,
+    image_url: dieffenbachiaPlantPot.src,
+    synonyms: [
+      "Dieffenbachia maculata",
+      "Caladium maculatum",
+      "Comigo-ninguém-pode"
+    ],
+    genus: "Dieffenbachia",
+    family: "Araceae",
+    links: {
+      self: "/api/v1/species/dieffenbachia-amoena",
+      plant: "/api/v1/plants/dieffenbachia-amoena",
+      genus: "/api/v1/genus/dieffenbachia"
+    },
+  },
 ];
