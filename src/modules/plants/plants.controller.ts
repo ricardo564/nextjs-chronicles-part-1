3554468cab78cde802140a706bb13b7daee82506
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 09:23:15 -0300

    Update PlantsController to use namespaced API route: Changed the controller's route from 'plants' to 'api/plants' for better API structure and clarity. This update enhances the organization of the API endpoints within the application.

--- src/modules/plants/plants.controller.ts
+++ src/modules/plants/plants.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { PlantsService } from './plants.service';
+import { PlantsService } from '@/modules/plants/plants.service';
 import { PlantApiResponse } from '@/types/plant.types';
 
-@Controller('plants')
+@Controller('api/plants')
 export class PlantsController {
   constructor(private readonly plantsService: PlantsService) {}
 
-  @Get('/')
+  @Get()
   async getPlants(): Promise<PlantApiResponse> {
     return this.plantsService.getPlants();
   }
