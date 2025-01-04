195babe5a4d8f216a24b8a1e737835b4a3ed4c8c
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-04 09:23:31 -0300

    Refactor PlantsService: Simplified API key management by removing ConfigService dependency and added error handling for plant data fetching. This change enhances the service's robustness and streamlines the retrieval process from the Trefle API.

--- src/modules/plants/plants.service.ts
+++ src/modules/plants/plants.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { HttpService } from '@nestjs/axios';
-import { ConfigService } from '@nestjs/config';
 import { PlantApiResponse } from '@/types/plant.types';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class PlantsService {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly configService: ConfigService,
-  ) {}
+  private readonly apiUrl = process.env.TREFLE_API_KEY;
+
+  constructor(private readonly httpService: HttpService) {}
 
   async getPlants(): Promise<PlantApiResponse> {
-    const apiKey = this.configService.get<string>('TREFLE_API_KEY');
-
-    const { data } = await this.httpService.axiosRef.get(
-      `https://trefle.io/api/v1/plants?token=${apiKey}`
-    );
-
-    return data as unknown as PlantApiResponse;
+    try {
+      const response = await this.httpService.axiosRef.get(
+        `https://trefle.io/api/v1/plants?token=${this.apiUrl}`
+      );
+      const data = response.data;
+      return data as unknown as PlantApiResponse;
+    } catch (error) {
+      console.error('Error fetching plants:', error);
+      throw error;
+    }
   }
 }
