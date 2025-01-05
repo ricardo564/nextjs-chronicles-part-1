413cfdce30f24f78d520fcd511bb6ad7188e3440
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 13:52:56 -0300

    Enhance MobileMenu component with close button and customizable class support
    - Added a close icon for improved user interaction when navigating the mobile menu.
    - Introduced a className prop to allow for customizable styling of the MobileMenu component.
    - Updated the layout to include a Logo and a close button, enhancing the overall mobile navigation experience.

--- src/components/mobileMenu.tsx
+++ src/components/mobileMenu.tsx
@@ -5,12 +5,15 @@ import { MenuItem } from "@/types/menuItem";
 import { useState } from "react";
 import Image from "next/image";
 import menuIcon from "@/assets/svg/menu-icon.svg";
+import closeIcon from "@/assets/svg/close-icon.svg";
+import { Logo } from "./Logo";
 
 interface MobileMenuProps {
   menuItems: MenuItem[];
+  className?: string;
 }
 
-export function MobileMenu({ menuItems }: MobileMenuProps) {
+export function MobileMenu({ menuItems, className }: MobileMenuProps) {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleToggleMenu = () => {
@@ -18,7 +21,7 @@ export function MobileMenu({ menuItems }: MobileMenuProps) {
   };
 
   return (
-    <div className="flex flex-col items-center space-y-4">
+    <div className={`flex flex-col items-center space-y-4 ${className}`}>
       <button
         className="flex items-center justify-between w-full"
         onClick={handleToggleMenu}
@@ -27,12 +30,20 @@ export function MobileMenu({ menuItems }: MobileMenuProps) {
       </button>
 
       <div
-        className={`flex-w-full absolute inset-y-0 left-0 w-full bg-primary/90 backdrop-blur-sm min-h-screen border-r border-white shadow-sm z-[6] max-w-xs ${
+        className={`flex-w-full absolute inset-y-0 left-0 w-full bg-primary/90 backdrop-blur-sm min-h-screen border-r border-white/20 shadow-sm z-[6] max-w-xs ${
           isOpen ? "translate-x-0" : "-translate-x-full"
         } transition-transform duration-300`}
       >
-          <div
-            className={`flex flex-col items-center space-y-4 min-w-[17rem] `}
+        <div className="flex items-center justify-between w-full">
+          <Logo />
+
+          <button className="flex items-center justify-center mr-4 mt-1 hover:bg-white/10 rounded-full p-2" onClick={handleToggleMenu}>
+            <Image src={closeIcon} alt="Close" width={24} height={24} />
+          </button>
+        </div>
+
+        <div
+          className={`flex flex-col items-center space-y-4 min-w-[17rem] `}
           >
             <ul className="flex flex-col items-center w-full mt-24">
               {menuItems.map((item) => (
