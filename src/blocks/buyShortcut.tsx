fdb300479e65b3460c9d8f3d94dc39ea6ad39acf
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 15:34:04 -0300

    Refactor BuyShortcut component to use an icon and remove label prop
    - Replaced the label text with an image icon for the BuyShortcut component, enhancing visual appeal.
    - Removed the unused label prop to simplify the component's interface.
    - This change improves the user experience by providing a more intuitive and visually engaging way to represent the purchasing action.

--- src/blocks/buyShortcut.tsx
+++ src/blocks/buyShortcut.tsx
@@ -1,20 +1,21 @@
 import { Plant } from "@/types/plant.types";
 import Link from "next/link";
+import Image from "next/image";
+import shopIcon from "@/assets/svg/shop-icon.svg";
 
 interface BuyShortcutProps {
   className?: string;
-  label?: string;
   plant: Plant;
 }
 
-export const BuyShortcut = ({ plant, className, label = "Buy" }: BuyShortcutProps) => {
+export const BuyShortcut = ({ plant, className }: BuyShortcutProps) => {
   return (
     <Link
       className={`px-6 py-2 border border-white/20 text-white rounded-lg min-w-[10rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Buy ${plant.common_name || plant.scientific_name} plants`}
       href={`/plants/${plant.id}`}
     >
-      {label}
+      <Image src={shopIcon} alt="Shop icon" width={26} height={26} />
     </Link>
   );
 };
