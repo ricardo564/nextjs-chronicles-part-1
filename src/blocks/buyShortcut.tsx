d885fb708abab9fef6006fbcc93f974d4228abb1
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 15:48:12 -0300

    Enhance BuyShortcut component to conditionally display icon or text
    - Added a new `showIcon` prop to the BuyShortcut component, allowing users to toggle the visibility of the shop icon.
    - Updated the component's rendering logic to display either the icon or a "Buy now" text based on the `showIcon` prop value.
    - This change improves flexibility and user experience by accommodating different design preferences.

--- src/blocks/buyShortcut.tsx
+++ src/blocks/buyShortcut.tsx
@@ -4,18 +4,26 @@ import Image from "next/image";
 import shopIcon from "@/assets/svg/shop-icon.svg";
 
 interface BuyShortcutProps {
-  className?: string;
   plant: Plant;
+  className?: string;
+  showIcon?: boolean;
 }
 
-export const BuyShortcut = ({ plant, className }: BuyShortcutProps) => {
+export const BuyShortcut = ({
+  plant,
+  className,
+  showIcon = true,
+}: BuyShortcutProps) => {
   return (
     <Link
       className={`px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Buy ${plant.common_name || plant.scientific_name} plants`}
       href={`/plants/${plant.id}`}
     >
-      <Image src={shopIcon} className="w-11 h-11" alt="Shop icon" />
+      {showIcon && (
+        <Image src={shopIcon} className="w-11 h-11" alt="Shop icon" />
+      )}
+      {!showIcon && <span className="text-sm">Buy now</span>}
     </Link>
   );
 };
