dacb474012668620948b3321b7f5bd46e988721a
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 15:37:25 -0300

    Refactor BuyShortcut component for improved styling and icon usage
    - Updated the BuyShortcut component to enhance its visual presentation by replacing the label text with a more prominent image icon.
    - Adjusted the icon's size for better alignment and aesthetics.
    - This change streamlines the component's interface and improves user experience by providing a clearer visual cue for purchasing actions.

--- src/blocks/buyShortcut.tsx
+++ src/blocks/buyShortcut.tsx
@@ -11,11 +11,11 @@ interface BuyShortcutProps {
 export const BuyShortcut = ({ plant, className }: BuyShortcutProps) => {
   return (
     <Link
-      className={`px-6 py-2 border border-white/20 text-white rounded-lg min-w-[10rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
+      className={`px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Buy ${plant.common_name || plant.scientific_name} plants`}
       href={`/plants/${plant.id}`}
     >
-      <Image src={shopIcon} alt="Shop icon" width={26} height={26} />
+      <Image src={shopIcon} className="w-11 h-11" alt="Shop icon" />
     </Link>
   );
 };
