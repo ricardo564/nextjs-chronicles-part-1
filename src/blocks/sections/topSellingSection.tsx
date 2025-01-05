ad9ddd3db6acf813cb4fb3cd78ff60f7ce922161
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 11:35:57 -0300

    Enhance Top Selling Section: Added QuotedTitle component for improved visual appeal and updated padding for better layout. This aligns with recent design updates and enhances the overall user interface consistency.

--- src/blocks/sections/topSellingSection.tsx
+++ src/blocks/sections/topSellingSection.tsx
@@ -1,9 +1,13 @@
+import { QuotedTitle } from "@/components/QuotedTitle";
+
 export default async function TopSellingSection() {
     return (
       <div
-        className="relative min-h-screen max-w-7xl mx-auto"
+        className="relative min-h-screen max-w-7xl mx-auto pt-32"
       >
-        <h1>Top Selling Section</h1>
+          <QuotedTitle className="text-center text-white">
+            Our Top Selling
+          </QuotedTitle>
       </div>
     )
 }
