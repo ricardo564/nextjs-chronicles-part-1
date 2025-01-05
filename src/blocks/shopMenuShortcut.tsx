1394eb6cd8ed6bc77174a7ccb500a6874e3caa68
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 12:16:00 -0300

    Add ShopMenuShortcut component and shop icon asset
    - Introduced a new ShopMenuShortcut component for easy navigation to the plants section.
    - Added a shop icon SVG asset to enhance visual representation in the ShopMenuShortcut.
    - The component supports customizable class names for improved styling flexibility.

---
+++ src/blocks/shopMenuShortcut.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import shopIcon from "@/assets/svg/shop-icon.svg";
+import Link from "next/link";
+
+interface ShopMenuShortcutProps {
+  className?: string;
+}
+
+export const ShopMenuShortcut = ({ className }: ShopMenuShortcutProps) => {
+  return (
+    <Link
+      className={`px-6 py-2 text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center flex items-center gap-2 ${className}`}
+      title="Shop"
+      href="/plants"
+    >
+      <Image src={shopIcon} alt="Shop icon" width={26} height={26} />
+    </Link>
+  );
+};
