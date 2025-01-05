b464240857da853a96db040ac387f6a490a02f29
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-05 11:19:09 -0300

    Refactor PlantCard component: Replaced BuyNowShortcut with BuyShortcut for improved customization and consistency. Adjusted layout for better responsiveness, including minor formatting changes for cleaner code. This enhances the user experience and maintains a cohesive design across the application.

--- src/components/PlantCard.tsx
+++ src/components/PlantCard.tsx
@@ -1,6 +1,6 @@
 import { Plant } from "@/types/plant.types";
 import Image from "next/image";
-import { BuyNowShortcut } from "./BuyNowShortcut";
+import { BuyShortcut } from "./BuyShortcut";
 
 interface PlantCardProps {
   plant: Plant;
@@ -15,20 +15,20 @@ export function PlantCard({
   imageClassName,
   titleClassName,
 }: PlantCardProps) {
-
   const getRandomName = () => {
     const synonymsLength = plant.synonyms.length;
 
     const randomIndex = Math.floor(Math.random() * synonymsLength);
 
     return plant.synonyms[randomIndex];
-  }
+  };
 
   return (
     <div
-      className={`relative bg-gradient-to-r from-white/10 via-transparent to-transparent backdrop-blur-md rounded-[32px] p-8 border border-white/20 w-full max-w-[20rem] flex flex-col items-center animate-fade-in ${containerClassName}`}
+      className={`relative bg-gradient-to-r from-white/10 via-transparent to-transparent backdrop-blur-md rounded-[32px] p-8 border border-white/20 w-full md:max-w-[20rem] flex flex-col items-center animate-fade-in ${containerClassName}`}
       role="article"
-      aria-label={`Plant card for ${plant.common_name || plant.scientific_name}`}
+      aria-label={`Plant card for ${plant.common_name || plant.scientific_name
+        }`}
     >
       <div className="mb-6 animate-slide-down">
         {plant.image_url && (
@@ -44,22 +44,26 @@ export function PlantCard({
       </div>
 
       <div className="flex flex-col w-full animate-slide-up">
-        <p className="text-gray-300 text-sm mb-2">
-          {getRandomName()}
-        </p>
+        <p className="text-gray-300 text-sm mb-2">{getRandomName()}</p>
 
         <p className="text-gray-300 text-xs grid grid-cols-2">
           <span>Family: {plant.family}</span>
           <span>Genus: {plant.genus}</span>
         </p>
 
-        <p className={`text-white text-2xl font-semibold my-3 h-32 w-full line-clamp-3 overflow-hidden  ${titleClassName}`}>
+        <p
+          className={`text-white text-2xl font-semibold my-3 h-32 w-full line-clamp-3 overflow-hidden  ${titleClassName}`}
+        >
           {plant.bibliography + " " + plant.scientific_name}
         </p>
       </div>
 
       <div className="w-full flex items-center justify-start animate-fade-in-delay">
-        <BuyNowShortcut plant={plant} />
+        <BuyShortcut
+          label="Buy Now"
+          className="px-6 py-3 bg-transparent backdrop-blur-sm border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300"
+          plant={plant}
+        />
       </div>
     </div>
   );
