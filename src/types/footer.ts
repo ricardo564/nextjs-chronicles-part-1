96d8cd7f7a505da3261933312f15eb8ae9241ae0
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 09:41:46 -0300

    Add footer.ts file defining FooterLink and SocialLink interfaces for improved site navigation

---
+++ src/types/footer.ts
@@ -0,0 +1,12 @@
+export interface FooterLink {
+  id: number;
+  label: string;
+  href: string;
+}
+
+export interface SocialLink {
+  id: number;
+  label: string;
+  href: string;
+  icon: string;
+}
