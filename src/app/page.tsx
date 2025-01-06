9520d27951e4d9cff12adc45153e780873b878e7
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 09:03:31 -0300

    Add TestimonialSection to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { IntroSection } from "@/blocks/sections/introSection";
 import TopSellingSection from "@/blocks/sections/topSellingSection";
 import DefaultLayout from "@/layouts/DefaultLayout";
+import { TestimonialSection } from "@/blocks/sections/testimonialSection";
 
 export default async function Home() {
   return (
     <DefaultLayout>
       <IntroSection />
       <TopSellingSection />
+      <TestimonialSection />
     </DefaultLayout>
   );
 }
