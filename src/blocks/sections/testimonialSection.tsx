9c1b0ec328418e744fed86cddbb53d6271c078f7
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 09:03:46 -0300

    Add TestimonialSection component for displaying customer reviews

---
+++ src/blocks/sections/testimonialSection.tsx
@@ -0,0 +1,25 @@
+import { QuotedTitle } from "@/components/QuotedTitle";
+import { testimonials } from "@/static/testimonials";
+import { TestimonialCard } from "@/components/TestimonialCard";
+
+export const TestimonialSection = () => {
+  return (
+    <div className="relative min-h-screen max-w-7xl mx-auto pt-32 px-2 h-full pb-6">
+      <div className="flex flex-col items-center max-w-[40rem] mx-auto">
+        <QuotedTitle className="text-center text-white">
+          Costumer Reviews
+        </QuotedTitle>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center h-auto w-full gap-[10rem] md:gap-y-[8rem] lg:gap-4 mt-[11rem] gap-y-[15rem]">
+        {testimonials.slice(0, 3).map((testimonial) => (
+          <TestimonialCard
+            key={testimonial.id}
+            testimonial={testimonial}
+            className="col-span-1 h-auto"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
