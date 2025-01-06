16ae46ca1dc078284630e98912cc02fa212997ba
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 15:57:05 -0300

    Refactor import paths in PlantCardContent component for consistency and clarity

--- src/components/plantCardContent.tsx
+++ src/components/plantCardContent.tsx
@@ -1,7 +1,7 @@
 import { Plant } from "@/types/plant.types";
 import Image from "next/image";
-import { BuyShortcut } from "../blocks/buyShortcut";
-import { ExploreShortcut } from "../blocks/exploreShortcut";
+import { BuyShortcut } from "@/blocks/buyShortcut";
+import { ExploreShortcut } from "@/blocks/exploreShortcut";
 
 interface PlantCardProps {
   plant: Plant;
