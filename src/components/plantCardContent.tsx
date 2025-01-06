1590beb9fe00d113214438ddda6c3ac4a70b3137
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 16:56:16 -0300

    Refactor PlantCardContent layout for improved structure and responsiveness. Updated the component to better align with flexbox principles, enhancing the visual presentation of plant details and ensuring consistent styling across elements.

--- src/components/plantCardContent.tsx
+++ src/components/plantCardContent.tsx
@@ -27,54 +27,54 @@ export function PlantCardContent({
   };
 
   return (
-    <>
-    <div className={`mb-6 animate-slide-down ${containerClassName}`}>
-      {plant.image_url && (
-        <Image
-          src={plant.image_url}
-          alt={plant.common_name || plant.scientific_name}
-          width={240}
-          height={240}
-          loading="lazy"
-          className={`-mt-24 md:-mt-32 object-cover w-[20rem] h-[20rem] rounded-2xl animate-scale-in scale-150 md:scale-125 ${imageClassName}`}
-        />
-      )}
-    </div>
-
-    <div className="flex flex-col w-full animate-slide-up">
-      <div className="flex flex-col w-full animate-slide-up">
-        <p className="text-white text-sm mb-2">{getRandomName()}</p>
-
-        <p className="text-white text-xs grid grid-cols-2">
-          <span>Family: {plant.family}</span>
-          <span>Genus: {plant.genus}</span>
-        </p>
-
-        <p
-          className={`text-white text-2xl font-semibold my-3 h-32 w-full line-clamp-3 overflow-hidden  ${titleClassName}`}
-        >
-          {plant.bibliography + " " + plant.scientific_name}
-        </p>
+    <div className={`flex flex-col items-center ${containerClassName}`}>
+      <div className={`mb-6 animate-slide-down `}>
+        {plant.image_url && (
+          <Image
+            src={plant.image_url}
+            alt={plant.common_name || plant.scientific_name}
+            width={240}
+            height={240}
+            loading="lazy"
+            className={`-mt-24 md:-mt-32 object-cover w-[20rem] h-[20rem] rounded-2xl animate-scale-in scale-150 md:scale-125 ${imageClassName}`}
+          />
+        )}
       </div>
 
-      {!showPrice && (
-        <div className="w-full flex items-center justify-start animate-fade-in-delay">
-          <BuyShortcut
-            className="px-6 py-3 bg-transparent backdrop-blur-sm border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300"
-            showIcon={false}
-            plant={plant}
-          />
+      <div className="flex flex-col w-full animate-slide-up">
+        <div className="flex flex-col w-full animate-slide-up">
+          <p className="text-white text-sm mb-2">{getRandomName()}</p>
+
+          <p className="text-white text-xs grid grid-cols-2">
+            <span>Family: {plant.family}</span>
+            <span>Genus: {plant.genus}</span>
+          </p>
+
+          <p
+            className={`text-white text-2xl font-semibold my-3 h-32 w-full line-clamp-3 overflow-hidden  ${titleClassName}`}
+          >
+            {plant.bibliography + " " + plant.scientific_name}
+          </p>
         </div>
+
+        {!showPrice && (
+          <div className="w-full flex items-center justify-start animate-fade-in-delay">
+            <BuyShortcut
+              className="px-6 py-3 bg-transparent backdrop-blur-sm border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300"
+              showIcon={false}
+              plant={plant}
+            />
+          </div>
         )}
 
         {showPrice && (
           <div className="flex items-center gap-4">
             <ExploreShortcut plant={plant} />
 
-          <BuyShortcut plant={plant} />
-        </div>
-      )}
+            <BuyShortcut plant={plant} />
+          </div>
+        )}
       </div>
-    </>
+    </div>
   );
 }
