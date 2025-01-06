1f1eccbf0b567389db9d5f1be55b23bac9dc6d86
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 09:21:32 -0300

    Add BestProductsSection to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { IntroSection } from "@/blocks/sections/introSection";
 import TopSellingSection from "@/blocks/sections/topSellingSection";
 import DefaultLayout from "@/layouts/DefaultLayout";
 import { TestimonialSection } from "@/blocks/sections/testimonialSection";
+import { BestProductsSection } from "@/blocks/sections/bestProductstSection";
 
 export default async function Home() {
   return (
@@ -9,6 +10,7 @@ export default async function Home() {
       <IntroSection />
       <TopSellingSection />
       <TestimonialSection />
+      <BestProductsSection />
     </DefaultLayout>
   );
 }
