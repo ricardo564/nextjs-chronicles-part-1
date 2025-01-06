8325d4d6b9957ef5bfaebc2d05525fc0f6ba95d8
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 09:21:37 -0300

    Add BestProductsSection component for showcasing top products

---
+++ src/blocks/sections/bestProductstSection.tsx
@@ -0,0 +1,26 @@
+import { QuotedTitle } from "@/components/QuotedTitle";
+import { mockupPlants } from "@/static/mockupPlants";
+import { PlantCard } from "@/components/PlantCard";
+
+export const BestProductsSection = () => {
+  return (
+    <div className="relative min-h-screen max-w-7xl mx-auto pt-32 px-2 h-full pb-6">
+      <div className="flex flex-col items-center max-w-[40rem] mx-auto">
+        <QuotedTitle className="text-center text-white">
+          Our Best o2
+        </QuotedTitle>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center h-auto min-h-[90rem] w-full gap-[10rem] md:gap-y-[8rem] lg:gap-4 mt-[11rem] gap-y-[15rem]">
+        {mockupPlants.slice(1, mockupPlants.length).map((plant) => (
+          <PlantCard
+            containerClassName="w-full max-w-full min-w-full"
+            key={plant.id}
+            plant={plant}
+            showPrice={false}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
