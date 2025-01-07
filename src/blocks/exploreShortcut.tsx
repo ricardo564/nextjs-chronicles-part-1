aa4f4f1caae33ebfe1c50b9004ec7914494035d7
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 00:48:17 -0300

    Enhance ExploreShortcut component layout by updating Link class for full-width flex display. This change improves alignment and user experience, ensuring better presentation across different screen sizes while maintaining design consistency.

--- src/blocks/exploreShortcut.tsx
+++ src/blocks/exploreShortcut.tsx
@@ -9,7 +9,7 @@ interface ExploreShortcutProps {
 export const ExploreShortcut = ({ plant, className }: ExploreShortcutProps) => {
   return (
     <Link
-      className={`px-6 py-2 border border-white/20 text-white rounded-lg min-w-[13rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
+      className={`w-full flex items-center justify-center px-6 py-2 border border-white/20 text-white rounded-lg min-w-[13rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Explore ${plant.common_name || plant.scientific_name} plants`}
       href='/plants'
     >
