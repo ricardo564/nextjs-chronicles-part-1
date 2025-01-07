7df57c23f1396891394aacbb5f7b11f739305928
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-06 21:22:58 -0300

    Enhance ExploreShortcut component by adding optional className prop. Updated the component's Link class to incorporate the new className, allowing for greater styling flexibility. This change aims to improve customization options for users and maintain design consistency across the application.

--- src/blocks/exploreShortcut.tsx
+++ src/blocks/exploreShortcut.tsx
@@ -3,12 +3,13 @@ import Link from "next/link";
 
 interface ExploreShortcutProps {
   plant: Plant;
+  className?: string;
 }
 
-export const ExploreShortcut = ({ plant }: ExploreShortcutProps) => {
+export const ExploreShortcut = ({ plant, className }: ExploreShortcutProps) => {
   return (
     <Link
-      className="px-6 py-2 border border-white/20 text-white rounded-lg min-w-[13rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center"
+      className={`px-6 py-2 border border-white/20 text-white rounded-lg min-w-[13rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Explore ${plant.common_name || plant.scientific_name} plants`}
       href='/plants'
     >
