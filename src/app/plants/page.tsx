af0f439eaa8eeefd22c8e1c0311f0c61b6f13c45
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 17:20:02 -0300

    Update PlantsPage to enhance PlantCard key generation for improved rendering
    This update modifies the PlantsPage component to include the index in the key prop of the PlantCard component. This change aims to improve the rendering performance and uniqueness of each PlantCard, ensuring better handling of dynamic lists in React.

--- src/app/plants/page.tsx
+++ src/app/plants/page.tsx
@@ -24,11 +24,11 @@ export default async function PlantsPage() {
             <NoDataToShow message="Nenhuma planta para ser exibida..." />
           ) : (
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 md:gap-6">
-              {plants.map((plant) => (
+              {plants.map((plant, index) => (
                 <PlantCard
                   containerClassName="w-full bg-white/5 backdrop-blur-sm rounded-2xl p-6 relative flex flex-col items-center shadow-lg hover:shadow-xl transition-all duration-300 border border-white/10"
                   imageClassName="rounded-full overflow-hidden w-[120px] h-[120px] md:w-[9rem] md:h-[9rem] -mt-12 md:-mt-[10rem] shadow-lg border-4 border-white/10 ml-12"
-                  key={plant.id}
+                  key={plant.id + index + "plant-card"}
                   plant={plant}
                   showPrice
                 />
