fdfb7fc02ae8546c271d864174ab831548434b3c
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 16:44:27 -0300

    Refactor ShoppingCart component to integrate QuantityShortcut for item management
    This update introduces the QuantityShortcut component into the ShoppingCart, allowing users to manage item quantities directly within the cart. The removeItem functionality has been removed in favor of the new component, streamlining the user interface. Additionally, button styles have been enhanced for better visual feedback during interactions. These changes aim to improve the overall user experience and interaction within the shopping cart.

--- src/blocks/shoppingCart.tsx
+++ src/blocks/shoppingCart.tsx
@@ -6,6 +6,7 @@ import shopIcon from "@/assets/svg/shop-icon.svg";
 import { handleWithBlockScroll } from "@/utils/handleWithBlockScroll";
 import CheckoutShortcut from "./checkoutShortcut";
 import { useShoppingCartStore } from "@/store/shoppingCartStore";
+import { QuantityShortcut } from "./quantityShortcut";
 
 interface ShoppingCartProps {
   className?: string;
@@ -14,7 +15,6 @@ interface ShoppingCartProps {
 export function ShoppingCart({ className }: ShoppingCartProps) {
   const [isOpen, setIsOpen] = useState(false);
   const { items: cartItems } = useShoppingCartStore();
-  const removeItem = useShoppingCartStore((state) => state.removeItem);
   const clearCart = useShoppingCartStore((state) => state.clearCart);
 
   const handleToggleMenu = () => {
@@ -89,33 +89,35 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
               Shopping Cart
             </p>
 
-            <button
-              className="text-white hover:text-red-500 rounded-full p-2 transition-all duration-300"
-              onClick={clearCart}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="34"
-                height="34"
-                viewBox="0 0 16 16"
+            <div className="flex flex-col gap-1">
+              <button
+                className="text-white hover:text-red-500 rounded-full p-2 transition-all duration-300"
+                onClick={clearCart}
               >
-                <g fill="none">
-                  <g clip-path="url(#gravityUiBroomMotion0)">
-                    <path
-                      fill="currentColor"
-                      fill-rule="evenodd"
-                      d="M11.995.667a.75.75 0 1 0-1.49.166L11.19 7h-.867c-1.64 0-2.896 1.449-3.197 3.06a12.6 12.6 0 0 1-1.2 3.44C5.434 14.448 5 15 5 15h8.5s2.08-1.734 2.488-5.49C16.14 8.094 14.91 7 13.488 7H12.7zM3.75 2.5a.75.75 0 1 0 0 1.5h4.5a.75.75 0 0 0 0-1.5zM.75 6a.75.75 0 1 0 0 1.5h5.5a.75.75 0 0 0 0-1.5zM1 10.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75m6.593 3.25c.393-.866.78-1.94 1.008-3.165C8.819 9.167 9.646 8.5 10.322 8.5h3.167c.332 0 .618.13.797.303a.63.63 0 0 1 .21.545c-.175 1.622-.708 2.779-1.173 3.514a6 6 0 0 1-.461.638h-.999c.539-.706.887-1.728.887-2.75H12c-.351 1.229-1.072 2.088-2.162 2.75z"
-                      clip-rule="evenodd"
-                    />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="34"
+                  height="34"
+                  viewBox="0 0 16 16"
+                >
+                  <g fill="none">
+                    <g clip-path="url(#gravityUiBroomMotion0)">
+                      <path
+                        fill="currentColor"
+                        fill-rule="evenodd"
+                        d="M11.995.667a.75.75 0 1 0-1.49.166L11.19 7h-.867c-1.64 0-2.896 1.449-3.197 3.06a12.6 12.6 0 0 1-1.2 3.44C5.434 14.448 5 15 5 15h8.5s2.08-1.734 2.488-5.49C16.14 8.094 14.91 7 13.488 7H12.7zM3.75 2.5a.75.75 0 1 0 0 1.5h4.5a.75.75 0 0 0 0-1.5zM.75 6a.75.75 0 1 0 0 1.5h5.5a.75.75 0 0 0 0-1.5zM1 10.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75m6.593 3.25c.393-.866.78-1.94 1.008-3.165C8.819 9.167 9.646 8.5 10.322 8.5h3.167c.332 0 .618.13.797.303a.63.63 0 0 1 .21.545c-.175 1.622-.708 2.779-1.173 3.514a6 6 0 0 1-.461.638h-.999c.539-.706.887-1.728.887-2.75H12c-.351 1.229-1.072 2.088-2.162 2.75z"
+                        clip-rule="evenodd"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="gravityUiBroomMotion0">
+                        <path fill="currentColor" d="M0 0h16v16H0z" />
+                      </clipPath>
+                    </defs>
                   </g>
-                  <defs>
-                    <clipPath id="gravityUiBroomMotion0">
-                      <path fill="currentColor" d="M0 0h16v16H0z" />
-                    </clipPath>
-                  </defs>
-                </g>
-              </svg>
-            </button>
+                </svg>
+              </button>
+            </div>
           </div>
 
           <div
@@ -153,30 +155,7 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
                     </span>
                   </div>
 
-                  <div className="w-[15%] flex justify-center">
-                    <button
-                      onClick={() => removeItem(cartItem.item.id)}
-                      className="p-2 hover:bg-white/10 rounded-full hover:text-red-500 transition-all duration-300"
-                      aria-label="Remove item"
-                      title="Remove item"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        strokeWidth="2"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      >
-                        <path d="M3 6h18" />
-                        <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-                        <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-                      </svg>
-                    </button>
-                  </div>
+                  <QuantityShortcut cartItem={cartItem} />
                 </div>
               ))
             ) : (
@@ -185,9 +164,7 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
           </div>
 
           <div className="fixed mt-auto bottom-0 w-full bg-transparent">
-            <CheckoutShortcut
-              className="w-full max-w-[17rem] mx-auto "
-            />
+            <CheckoutShortcut className="w-full max-w-[17rem] mx-auto " />
           </div>
         </div>
       </div>
