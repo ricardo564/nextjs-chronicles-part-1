f2592bec4c24e0ba2cb389209a27650f38d21efa
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 16:22:44 -0300

    Refactor CheckoutShortcut component to utilize shopping cart state management. Removed hardcoded total value and replaced it with a dynamic total calculated from cart items, enhancing integration with the shopping cart store.

--- src/blocks/checkoutShortcut.tsx
+++ src/blocks/checkoutShortcut.tsx
@@ -1,14 +1,18 @@
+import { useShoppingCartStore } from "@/store/shoppingCartStore";
+
 interface CheckoutShortcutProps {
-  total: number;
   className?: string;
 }
 
-export default function CheckoutShortcut({ total = 0, className }: CheckoutShortcutProps) {
+export default function CheckoutShortcut({ className }: CheckoutShortcutProps) {
+  const { items: cartItems } = useShoppingCartStore();
+  const totalOnStore = cartItems.reduce((acc, cartItem) => acc + cartItem.item.genus_id * cartItem.quantity, 0);
+
   return (
     <div className={`flex items-center justify-center w-full bottom-12 -mt-6 ${className}`}>
     <button className="bg-white text-primary hover:text-white px-4 py-2 w-full grid grid-cols-2 gap-2 border border-white/20 rounded-lg hover:bg-white/10 transition-all duration-300 text-center">
       <span className="font-bold">
-        ${total.toFixed(2)}
+        ${totalOnStore.toFixed(2)}
       </span>
       <span className="text-md font-bold">
         Checkout
