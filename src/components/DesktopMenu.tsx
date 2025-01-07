93f52ae711a56f537163c59aa4ba2ef76b37841a
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 17:20:51 -0300

    Refactor DesktopMenu component to improve key generation for menu items
    This update enhances the key generation for menu items within the DesktopMenu component. The keys now combine the item's label with the index and a specific suffix, ensuring uniqueness and improving rendering performance in dynamic lists. This change aligns with best practices for React component rendering.

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -9,8 +9,8 @@ interface DesktopMenuProps {
 export function DesktopMenu({ menuItems, className }: DesktopMenuProps) {
   return (
     <ul className={`hidden md:flex items-center space-x-8 ${className}`}>
-      {menuItems.map((item) => (
-        <li key={item.label}>
+      {menuItems.map((item, index) => (
+        <li key={item.label + index + "desktop-menu"}>
           <Link
             href={item.to}
             aria-label={item.ariaLabel}
