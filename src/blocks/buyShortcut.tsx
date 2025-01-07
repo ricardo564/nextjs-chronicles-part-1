f0fd51ff5f8ea3281acdac18952d34cc252cf573
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 16:51:32 -0300

    Refactor BuyShortcut component to integrate QuantityShortcut for item management
    This update enhances the BuyShortcut component by introducing a check for existing items in the shopping cart. If the item is already in the cart, it displays the QuantityShortcut component, allowing users to manage item quantities directly. If not, a button is provided to add the item to the cart. This change aims to improve user interaction and streamline the shopping experience.

--- src/blocks/buyShortcut.tsx
+++ src/blocks/buyShortcut.tsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 
 import { Plant } from "@/types/plant.types";
 import Image from "next/image";
 import shopIcon from "@/assets/svg/shop-icon.svg";
 import { useShoppingCartStore } from "@/store/shoppingCartStore";
+import { QuantityShortcut } from "./quantityShortcut";
 
 interface BuyShortcutProps {
   plant: Plant;
@@ -16,19 +17,32 @@ export const BuyShortcut = ({
   className,
   showIcon = true,
 }: BuyShortcutProps) => {
-
   const { addItem } = useShoppingCartStore();
 
-  return (
+  const itemExistsOnCart = () => {
+    const cartItems = useShoppingCartStore.getState().items;
+    return cartItems.some((item) => item.item.id === plant.id);
+  };
+
+  return itemExistsOnCart() ? (
+    <QuantityShortcut
+      cartItem={{ item: plant, quantity: 1 }}
+      className={`w-full md:w-auto !flex-row px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center mr-auto`}
+    />
+  ) : (
     <button
       onClick={() => addItem(plant)}
       type="button"
-      className={`w-full md:w-auto flex items-center justify-center px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
-      title={`Buy ${plant.common_name || plant.scientific_name} plants`}
-    >
-      {showIcon && (
-        <Image src={shopIcon} className="w-11 h-11 max-w-[44px] max-h-[44px]" alt="Shop icon" />
-      )}
+        className={`w-full md:w-auto flex items-center justify-center px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
+        title={`Buy ${plant.common_name || plant.scientific_name} plants`}
+      >
+        {showIcon && (
+          <Image
+            src={shopIcon}
+            className="w-11 h-11 max-w-[44px] max-h-[44px]"
+            alt="Shop icon"
+          />
+        )}
       {!showIcon && <span className="text-sm">Buy now</span>}
     </button>
   );
