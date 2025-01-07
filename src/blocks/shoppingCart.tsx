6206746c41e6eef9325fb180b0d8d917b77318eb
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-07 19:44:36 -0300

    Rename "Remove" button to "Clear" in ShoppingCart component for clarity

--- src/blocks/shoppingCart.tsx
+++ src/blocks/shoppingCart.tsx
@@ -115,7 +115,7 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
                     onClick={() => handleClearCartConfirm()}
                     className="bg-red-500 text-white px-4 py-2 rounded-md w-full"
                   >
-                    Remove
+                    Clear
                   </button>
                 </div>
               </div>
