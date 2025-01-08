67a15c77c9248496ecce39859be4be7c5308d6ae
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 15:07:47 -0300

    Refactor IntroSection component for improved layout and responsiveness
    - Updated the grid layout of the shortcuts section to use a single column on smaller screens and two columns on medium and larger screens, enhancing responsiveness.
    - Adjusted the maximum width for better layout consistency across different screen sizes.
    - These changes aim to improve the overall user experience and maintainability of the IntroSection component, aligning with recent design updates.

--- src/blocks/sections/introSection.tsx
+++ src/blocks/sections/introSection.tsx
@@ -48,7 +48,7 @@ export async function IntroSection() {
             </p>
           </div>
 
-          <div className="grid grid-cols-2 gap-4 max-w-[23rem]">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:max-w-[23rem]">
             <ExploreShortcut
               plant={mockupPlants[1]}
               className="w-full"
