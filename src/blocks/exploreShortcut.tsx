a265aecafa25aeaa84a4fe4d7d41bbfc8139b730
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 15:04:27 -0300

    Refactor ExploreShortcut component for improved layout and responsiveness
    - Updated the class name to include a minimum width and prevent shrinking, enhancing layout consistency.
    - These changes aim to improve the overall user experience and maintainability of the ExploreShortcut component, aligning with recent design updates.

--- src/blocks/exploreShortcut.tsx
+++ src/blocks/exploreShortcut.tsx
@@ -9,7 +9,7 @@ interface ExploreShortcutProps {
 export const ExploreShortcut = ({ plant, className }: ExploreShortcutProps) => {
   return (
     <Link
-      className={`w-full flex items-center justify-center px-5 py-2 border border-white/20 text-white rounded-lg md:w-[12rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
+      className={`flex-shrink-0 min-w-full w-full flex items-center justify-center md:max-w-[13rem] px-5 py-2 border border-white/20 text-white rounded-lg md:w-[12rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
       title={`Explore ${plant.common_name || plant.scientific_name} plants`}
       href='/plants'
     >
