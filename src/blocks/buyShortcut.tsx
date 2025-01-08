76d566518aca3933be032553a8c2397b1dacff28
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 10:35:58 -0300

    Revert "Refactor BuyShortcut component to optimize cart item checks and improve rendering logic"
    This reverts commit fa6296cbdcf63b8ae58d71d2bfdfa21c7a242e53.

--- src/blocks/buyShortcut.tsx
+++ src/blocks/buyShortcut.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useMemo } from "react";
 import { Plant } from "@/types/plant.types";
 import Image from "next/image";
 import shopIcon from "@/assets/svg/shop-icon.svg";
@@ -24,41 +23,31 @@ export const BuyShortcut = ({
 }: BuyShortcutProps) => {
   const { addItem } = useShoppingCartStore();
 
-  const itemExistsOnCart = useMemo(() => {
+  const itemExistsOnCart = () => {
     const cartItems = useShoppingCartStore.getState().items;
     return cartItems.some((item) => item.item.id === plant.id);
-  }, [plant.id]);
+  };
 
-  const cartItemData = useMemo(() => {
-    if (!itemExistsOnCart) return null;
-
-    return { item: plant, quantity: 1 };
-  }, [itemExistsOnCart, plant]);
-
-  if (itemExistsOnCart && cartItemData) {
-    return (
-      <QuantityShortcut
-        cartItem={cartItemData}
-        className={`w-full md:w-auto !flex-row px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center mr-auto ${quantityClassName}`}
-        showRemoveButton={showRemoveButton}
-      />
-    );
-  }
-
-  return (
+  return itemExistsOnCart() ? (
+    <QuantityShortcut
+      cartItem={{ item: plant, quantity: 1 }}
+      className={`w-full md:w-auto !flex-row px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center mr-auto ${quantityClassName}`}
+      showRemoveButton={showRemoveButton}
+    />
+  ) : (
     <button
       onClick={() => addItem(plant)}
       type="button"
-      className={`w-full md:w-auto flex items-center justify-center px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
-      title={`Buy ${plant.common_name || plant.scientific_name} plants`}
-    >
-      {showIcon && (
-        <Image
-          src={shopIcon}
-          className="w-11 h-11 max-w-[44px] max-h-[44px]"
-          alt="Shop icon"
-        />
-      )}
+        className={`w-full md:w-auto flex items-center justify-center px-6 py-2 border border-white/20 text-white rounded-lg hover:bg-white/10 transition-all duration-300 text-center ${className}`}
+        title={`Buy ${plant.common_name || plant.scientific_name} plants`}
+      >
+        {showIcon && (
+          <Image
+            src={shopIcon}
+            className="w-11 h-11 max-w-[44px] max-h-[44px]"
+            alt="Shop icon"
+          />
+        )}
       {!showIcon && <span className="text-sm">Buy now</span>}
     </button>
   );
