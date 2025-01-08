6e2a0550e854f94a324c0a5e12eebecec6346ac8
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 18:41:26 -0300

    Update menuItems to change navigation links for "Plants Types" and "Contact" to "#" for improved routing management

--- src/static/menuItems.ts
+++ src/static/menuItems.ts
@@ -2,7 +2,7 @@ import { MenuItem } from "@/types/menuItem";
 
 export const menuItems: MenuItem[] = [
   { label: "Home", to: "/", ariaLabel: "Home" },
-  { label: "Plants Types", to: "/plants-types", ariaLabel: "Plants Types" },
+  { label: "Plants Types", to: "#", ariaLabel: "Plants Types" },
   { label: "More", to: "/plants", ariaLabel: "More" },
-  { label: "Contact", to: "/contact", ariaLabel: "Contact" },
+  { label: "Contact", to: "#", ariaLabel: "Contact" },
 ];
