c68264a477d967866cb1fb1d479090577ee3fdfa
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 16:49:50 -0300

    Update ShoppingCart button styling for improved visibility
    - Changed the "Clear" button background color to red for better visibility and user attention.
    - This update enhances the user experience by making the button more prominent, aligning with recent design improvements.

--- src/blocks/shoppingCart.tsx
+++ src/blocks/shoppingCart.tsx
@@ -144,7 +144,7 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
                   </button>
                   <button
                     onClick={() => handleClearCartConfirm()}
-                    className=" text-white px-4 py-2 rounded-md w-full"
+                    className=" text-white bg-red-500 px-4 py-2 rounded-md w-full"
                   >
                     Clear
                   </button>
