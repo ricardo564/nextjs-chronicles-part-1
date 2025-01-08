5f86cb1a4f743e505f0b3c89b723ae71a5d0568c
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-08 15:03:58 -0300

    Refactor ShoppingCart component button styling for improved consistency
    - Updated the class name of the "Clear" button to remove the background color, aligning it with the overall design language.
    - This change aims to enhance the visual consistency and user experience of the ShoppingCart component, following recent updates for better design integration.

--- src/blocks/shoppingCart.tsx
+++ src/blocks/shoppingCart.tsx
@@ -144,7 +144,7 @@ export function ShoppingCart({ className }: ShoppingCartProps) {
                   </button>
                   <button
                     onClick={() => handleClearCartConfirm()}
-                    className="bg-red-500 text-white px-4 py-2 rounded-md w-full"
+                    className=" text-white px-4 py-2 rounded-md w-full"
                   >
                     Clear
                   </button>
