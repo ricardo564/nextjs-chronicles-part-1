f51a3e4f0f0c8f75915bde567218fc08ac26897d
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-09 07:48:07 -0300

    Refactor CheckoutShortcut component to integrate Button component and add loading state
    - Replaced the native button with a custom Button component for improved styling and functionality.
    - Introduced a loading prop to manage the button's state during asynchronous operations.
    - Updated the component structure to enhance user interaction and maintain visual consistency.
    
    These changes aim to improve the overall user experience and align with recent design updates.

--- src/blocks/checkoutShortcut.tsx
+++ src/blocks/checkoutShortcut.tsx
@@ -1,23 +1,28 @@
+import Button from "@/components/Button";
 import { useShoppingCartStore } from "@/store/shoppingCartStore";
 
 interface CheckoutShortcutProps {
   className?: string;
+  loading?: boolean;
 }
 
-export default function CheckoutShortcut({ className }: CheckoutShortcutProps) {
+export default function CheckoutShortcut({ className, loading }: CheckoutShortcutProps) {
   const { items: cartItems } = useShoppingCartStore();
   const totalOnStore = cartItems.reduce((acc, cartItem) => acc + cartItem.item.genus_id * cartItem.quantity, 0);
 
   return (
     <div className={`flex items-center justify-center w-full bottom-12 -mt-6 ${className}`}>
-    <button className="bg-white text-primary hover:text-white px-4 py-2 w-full grid grid-cols-2 gap-2 border border-white/20 rounded-lg hover:bg-white/10 transition-all duration-300 text-center">
+    <Button
+      loading={loading}
+      className="bg-white !text-black hover:text-white px-4 py-2 w-full grid grid-cols-2 gap-2 border border-white/20 rounded-lg hover:bg-white/10 transition-all duration-300 text-center"
+    >
       <span className="font-bold">
         ${totalOnStore.toFixed(2)}
       </span>
       <span className="text-md font-bold">
         Checkout
       </span>
-    </button>
+    </Button>
   </div>
   )
 }
