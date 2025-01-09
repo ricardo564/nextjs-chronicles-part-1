e6c9db01d8443c6d311512dd774511a3791e141d
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-09 09:05:32 -0300

    Refactor SearchShortcut component to use Button and DropdownSelect for enhanced user interaction
    - Replaced the Link component with a Button for better accessibility and interaction.
    - Added a DropdownSelect for search options, improving user experience.
    - Implemented click outside detection to close the dropdown, enhancing usability.
    - Updated styling to maintain responsiveness and visual consistency.

--- src/blocks/searchShortcut.tsx
+++ src/blocks/searchShortcut.tsx
@@ -1,27 +1,75 @@
-import Link from "next/link";
+"use client";
+
 import Image from "next/image";
 import searchIcon from "@/assets/svg/search-icon.svg";
+import DropdownSelect from "@/components/DropdownSelect";
+import { useEffect, useRef, useState } from "react";
+import Button from "@/components/Button";
+import { isClickOutsideElement } from "@/utils/isClickOutsideElement";
 
 interface SearchShortcutProps {
   className?: string;
   label?: string;
 }
 
+const options = [
+  { label: "Plants", value: "plants" },
+  { label: "Species", value: "species" },
+  { label: "Genus", value: "genus" },
+  { label: "Family", value: "family" },
+];
+
 export const SearchShortcut = ({ className, label }: SearchShortcutProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const dropdownListRef = useRef<HTMLDivElement>(null);
+
+  function handleToggle() {
+    setIsOpen(!isOpen);
+  }
+
+  function handleClickOutside(event: MouseEvent) {
+    const clickedOutsideDropdownList = isClickOutsideElement(dropdownListRef.current, event);
+
+    if (clickedOutsideDropdownList) {
+      setIsOpen(false);
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("click", handleClickOutside);
+    } else {
+      document.removeEventListener("click", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <Link
-      className={`px-6 py-2  text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center flex items-center gap-2 ${className}`}
-      title={`Search for plants`}
-      href={`/plants`}
-    >
-      <Image
-        alt="Search icon"
-        className="md:w-11 md:h-11 max-w-[44px] max-h-[44px]"
-        width={26}
-        height={26}
-        src={searchIcon}
-      />
-      {label}
-    </Link>
+    <>
+      <Button
+        className={`px-6 py-2  text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center flex items-center gap-2 border-none ${className}`}
+        title={`Search for plants`}
+        onClick={handleToggle}
+      >
+        <Image
+          alt="Search icon"
+          className="md:w-11 md:h-11 max-w-[44px] max-h-[44px]"
+          width={26}
+          height={26}
+          src={searchIcon}
+        />
+        {label}
+      </Button>
+
+      <div
+        ref={dropdownListRef}
+        className={`absolute left-0 top-[4rem] pr-2 min-w-[99vw] bg-white/20 backdrop-blur-sm z-[6] text-white border border-white/20 rounded-lg h-10 ${isOpen ? "block" : "hidden"}`}
+      >
+        <DropdownSelect name="searchShortcut" options={options} />
+      </div>
+    </>
   );
 };
