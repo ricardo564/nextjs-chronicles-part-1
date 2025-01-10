5a0e8454a2064cacd9457e81eb0db6103b000ac8
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-10 18:23:11 -0300

    Enhance Label component to support 'htmlFor' prop for improved accessibility

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -3,12 +3,13 @@ import type { FC } from "react";
 interface Props {
   value: string;
   className?: string;
+  htmlFor?: string;
 }
 
-const Label: FC<Props> = ({ value, className }: Props) => {
+const Label: FC<Props> = ({ value, className, htmlFor }: Props) => {
   return (
     <div className={className}>
-      <label className="d-label">
+      <label className="d-label" htmlFor={htmlFor}>
         <span className="d-label-text text-[1rem] font-medium">{value}</span>
       </label>
     </div>
