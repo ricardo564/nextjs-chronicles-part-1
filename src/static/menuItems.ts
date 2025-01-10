923827dc8b4c81e1beda9217cbc89de6c47037ad
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-10 18:37:43 -0300

    Update Contact menu item link to point to the correct route

--- src/static/menuItems.ts
+++ src/static/menuItems.ts
@@ -4,5 +4,5 @@ export const menuItems: MenuItem[] = [
   { label: "Home", to: "/", ariaLabel: "Home" },
   { label: "Plants Types", to: "#", ariaLabel: "Plants Types" },
   { label: "More", to: "/plants", ariaLabel: "More" },
-  { label: "Contact", to: "#", ariaLabel: "Contact" },
+  { label: "Contact", to: "/contact", ariaLabel: "Contact" },
 ];
