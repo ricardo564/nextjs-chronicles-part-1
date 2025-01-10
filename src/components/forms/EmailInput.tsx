68ed3005e3632ccfd1742e71e97681611a1c4d9f
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-10 18:34:16 -0300

    Refactor EmailInput component to improve code readability and enhance label styling

--- src/components/forms/EmailInput.tsx
+++ src/components/forms/EmailInput.tsx
@@ -1,16 +1,21 @@
-import type { FC } from 'react'
-import { FieldValues, UseFormRegister, RegisterOptions, Path } from 'react-hook-form'
-import Label from '@/components/Label'
+import type { FC } from "react";
+import {
+  FieldValues,
+  UseFormRegister,
+  RegisterOptions,
+  Path,
+} from "react-hook-form";
+import Label from "@/components/Label";
 
 interface Props {
-  name: string
-  label?: string
-  className?: string
-  placeholder?: string
-  disabled?: boolean
-  error?: string
-  register: UseFormRegister<FieldValues>
-  rules?: RegisterOptions<FieldValues, Path<FieldValues>>
+  name: string;
+  label?: string;
+  className?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  error?: string;
+  register: UseFormRegister<FieldValues>;
+  rules?: RegisterOptions<FieldValues, Path<FieldValues>>;
 }
 
 export const EmailInput: FC<Props> = ({
@@ -21,20 +26,26 @@ export const EmailInput: FC<Props> = ({
   disabled = false,
   error,
   register,
-  rules
+  rules,
 }: Props) => {
   return (
     <div className={`flex flex-col ${className}`}>
-      {label && <Label value={label} htmlFor={name} />}
+      {label && (
+        <Label
+          className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent  rounded-full"
+          value={label}
+          htmlFor={name}
+        />
+      )}
       <input
         {...register(name, rules)}
         type="email"
         id={name}
         placeholder={placeholder}
         disabled={disabled}
-        className="w-full px-4 py-3 rounded-lg bg-gray-700/50 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition"
+        className="z-[2] w-full px-4 py-3 rounded-lg bg-gray-700 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition"
       />
       {error && <p className="mt-1 text-sm text-red-400">{error}</p>}
     </div>
-  )
-}
+  );
+};
