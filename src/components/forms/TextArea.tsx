66df62bfb0de431f76020eaf630390fce6ca7af2
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-10 18:34:25 -0300

    Refactor TextArea component to improve code readability and enhance label styling

--- src/components/forms/TextArea.tsx
+++ src/components/forms/TextArea.tsx
@@ -1,17 +1,22 @@
-import { FC } from 'react'
-import { FieldValues, Path, RegisterOptions, UseFormRegister } from 'react-hook-form'
-import Label from '@/components/Label'
+import { FC } from "react";
+import {
+  FieldValues,
+  Path,
+  RegisterOptions,
+  UseFormRegister,
+} from "react-hook-form";
+import Label from "@/components/Label";
 
 interface Props {
-  name: string
-  label?: string
-  className?: string
-  placeholder?: string
-  disabled?: boolean
-  error?: string
-  register: UseFormRegister<FieldValues>
-  rules?: RegisterOptions<FieldValues, Path<FieldValues>>
-  rows?: number
+  name: string;
+  label?: string;
+  className?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  error?: string;
+  register: UseFormRegister<FieldValues>;
+  rules?: RegisterOptions<FieldValues, Path<FieldValues>>;
+  rows?: number;
 }
 
 export const TextArea: FC<Props> = ({
@@ -23,20 +28,26 @@ export const TextArea: FC<Props> = ({
   error,
   register,
   rules,
-  rows = 4
+  rows = 4,
 }: Props) => {
   return (
     <div className={`flex flex-col ${className}`}>
-      {label && <Label value={label} htmlFor={name} />}
+      {label && (
+        <Label
+          className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent  rounded-full"
+          value={label}
+          htmlFor={name}
+        />
+      )}
       <textarea
         {...register(name, rules)}
         id={name}
         rows={rows}
         placeholder={placeholder}
         disabled={disabled}
-        className="w-full px-4 py-3 rounded-lg bg-gray-700/50 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition resize-none"
+        className="z-[2] w-full px-4 py-3 rounded-lg bg-gray-700 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-transparent transition resize-none"
       />
       {error && <p className="mt-1 text-sm text-red-400">{error}</p>}
     </div>
-  )
-}
+  );
+};
