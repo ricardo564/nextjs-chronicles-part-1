a1f54912de3ee2e569a95a5d7508b6eb8c34f7bc
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-10 13:41:17 -0300

    Integrate Google Tag Manager and Analytics component into layout
    - Added Google Tag Manager script for enhanced tracking capabilities.
    - Included a noscript fallback for users with JavaScript disabled.
    - Integrated the Analytics component to streamline data collection and analysis.
    - Improved overall layout structure for better performance and user experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/assets/styles/globals.css";
 import ogImage from "@/assets/images/android-launchericon-512-512.png";
 import Script from "next/script";
 import IntroWarningModal from "@/blocks/IntroWarningModal";
+import { Analytics } from '@/components/Analytics'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -51,11 +52,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_ID}');
+            `
+          }}
+        />
+      </head>
       <body
         className={`scroll-smooth	${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <IntroWarningModal />
         {children}
+        <Analytics />
       </body>
 
       <Script
