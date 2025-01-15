c40f305427738f6d187076241377751f275e8dca
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-15 18:54:48 -0300

    Add dynamic background images and titles to ContactPage for enhanced visual appeal

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -2,6 +2,10 @@ import type { Metadata } from "next";
 import { ContactForm } from '@/blocks/ContactForm'
 import ogImage from '@/assets/images/android-launchericon-512-512.png'
 import DefaultLayout from "@/layouts/DefaultLayout";
+import bgBonsai from '@/assets/images/bonsai.webp'
+import bgCactus from '@/assets/images/cactus.webp'
+import bgJibola from '@/assets/images/jibola.webp'
+import Image from 'next/image'
 
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_SITE_URL || ''),
@@ -31,8 +35,44 @@ export const metadata: Metadata = {
 };
 
 export default function ContactPage() {
+  const backgrounds = [
+    {
+      id: 1,
+      image: bgBonsai,
+      title: "BONSAI",
+    },
+    {
+      id: 2,
+      image: bgCactus,
+      title: "CACTUS",
+    },
+    {
+      id: 3,
+      image: bgJibola,
+      title: "JIBOIA",
+    },
+  ]
+
+  const randomBackground = backgrounds[Math.floor(Math.random() * backgrounds.length)]
+
   return (
     <DefaultLayout>
+      <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-[#225348] to-[#8DB575] absolute inset-0 min-h-full blur-sm">
+
+        <p
+          className="text-white text-7xl font-bold absolute top-[7rem] left-6"
+        >
+          {randomBackground.title}
+        </p>
+
+        <Image
+          src={randomBackground.image}
+          alt="Background"
+          className="absolute right-0 ml-auto bottom-0 min-h-full w-full object-cover max-w-[20rem] max-h-screen overflow-visible"
+          fill
+        />
+      </div>
+
       <ContactForm
         className="mt-8"
       />
