ef851ffce5add018af419457b23a96d0eac147f2
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-15 18:31:38 -0300

    Refactor Home component to use Loading component in Suspense fallback
    - Replaced the static loading message with the new Loading component for improved user experience during content loading.
    - This change enhances the visual feedback provided to users while the page content is being fetched.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,10 +4,13 @@ import DefaultLayout from "@/layouts/DefaultLayout";
 import { TestimonialSection } from "@/blocks/sections/testimonialSection";
 import { BestProductsSection } from "@/blocks/sections/bestProductstSection";
 import { Suspense } from 'react'
+import { Loading } from "@/components/Loading";
 
 export default function Home() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={
+      <Loading />
+    }>
       <DefaultLayout>
         <IntroSection />
         <TopSellingSection />
