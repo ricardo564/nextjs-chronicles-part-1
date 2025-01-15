5b86d37e4ef9bfced4716dbda877ecb420f0045c
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-15 19:06:18 -0300

    Refactor PlantsPage to include loading state with Suspense component
    - Wrapped the PlantsPage component in a Suspense fallback to display a loading indicator while fetching plant data.
    - Imported and utilized the Loading component to enhance user experience during data retrieval.
    - This change improves the overall responsiveness of the PlantsPage by providing immediate feedback to users while content is being loaded.

--- src/app/[locale]/plants/page.tsx
+++ src/app/[locale]/plants/page.tsx
@@ -6,6 +6,8 @@ import { PlantsService } from "@/modules/plants/plants.service";
 import { HttpService } from "@nestjs/axios";
 import { getUniqueId } from "@/utils/getUniqueId";
 import { useMockupPlants } from "@/hooks/mockupPlants";
+import { Loading } from "@/components/Loading";
+import { Suspense } from "react";
 
 export default function PlantsPage() {
   const httpService = new HttpService();
@@ -29,28 +31,32 @@ export default function PlantsPage() {
     }
 
     return (
-      <DefaultLayout>
-        <div className="py-8 px-2 md:pt-[11rem]">
-          {plants.length === 0 ? (
-            <NoDataToShow message="Nenhuma planta para ser exibida..." />
-          ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-16 md:gap-6 md:gap-y-[12rem]">
-              {plants.map((plant, index) => (
-                <PlantCard
-                  containerClassName="w-full bg-white/5 backdrop-blur-sm rounded-2xl p-6 relative flex flex-col items-center shadow-lg hover:shadow-xl transition-all duration-300 border border-white/10"
-                  imageClassName="rounded-full overflow-hidden !w-[120px] !h-[120px] md:!w-[9rem] md:!h-[9rem] !-mt-[2.5rem] md:!-mt-[10rem] shadow-lg border-4 border-white/10 ml-12"
-                  shopIconClassName="min-w-[17rem] w-full"
-                  quantityClassName="!min-w-[17rem] !w-full md:!ml-[11rem]"
-                  key={`${plant.id}-${index}-plant-card-${getUniqueId()}`}
-                  plant={plant}
-                  showExploreShortcut={false}
-                  showPrice
-                />
-              ))}
-            </div>
-          )}
-        </div>
-      </DefaultLayout>
+      <Suspense fallback={
+        <Loading />
+      }>
+        <DefaultLayout>
+          <div className="py-8 px-2 md:pt-[11rem]">
+            {plants.length === 0 ? (
+              <NoDataToShow message="Nenhuma planta para ser exibida..." />
+            ) : (
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-16 md:gap-6 md:gap-y-[12rem]">
+                {plants.map((plant, index) => (
+                  <PlantCard
+                    containerClassName="w-full bg-white/5 backdrop-blur-sm rounded-2xl p-6 relative flex flex-col items-center shadow-lg hover:shadow-xl transition-all duration-300 border border-white/10"
+                    imageClassName="rounded-full overflow-hidden !w-[120px] !h-[120px] md:!w-[9rem] md:!h-[9rem] !-mt-[2.5rem] md:!-mt-[10rem] shadow-lg border-4 border-white/10 ml-12"
+                    shopIconClassName="min-w-[17rem] w-full"
+                    quantityClassName="!min-w-[17rem] !w-full md:!ml-[11rem]"
+                    key={`${plant.id}-${index}-plant-card-${getUniqueId()}`}
+                    plant={plant}
+                    showExploreShortcut={false}
+                    showPrice
+                  />
+                ))}
+              </div>
+            )}
+          </div>
+        </DefaultLayout>
+      </Suspense>
     );
   } catch (error: unknown) {
     console.error("Error fetching plants:", error);
