7ff0a593d0b35f43b9a8f88f457e0a59f6f7e980
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-15 12:09:14 -0300

    Refactor ExploreShortcut component for improved internationalization
    - Updated translation keys in the ExploreShortcut component to align with the new 'explore' namespace.
    - Modified title and link text to utilize the updated translation keys for better localization.
    - Ensured consistency in text rendering by using the correct translation functions.

--- src/blocks/exploreShortcut.tsx
+++ src/blocks/exploreShortcut.tsx
@@ -8,15 +8,15 @@ interface ExploreShortcutProps {
 }
 
 export const ExploreShortcut = ({ plant, className }: ExploreShortcutProps) => {
-  const t = useTranslations('hero');
+  const t = useTranslations('explore');
 
   return (
     <Link
       className={`flex-shrink-0 min-w-full w-full flex items-center justify-center md:max-w-[13rem] px-5 py-2 border border-white/20 text-white rounded-lg md:w-[12rem] text-[1.75rem] hover:bg-white/10 transition-all duration-300 text-center ${className}`}
-      title={t('explore.title', { name: plant.common_name || plant.scientific_name })}
+      title={t('title', { name: plant.common_name || plant.scientific_name })}
       href='/plants'
     >
-      {t('explore.text')}
+      {t('text')}
     </Link>
   );
 };
