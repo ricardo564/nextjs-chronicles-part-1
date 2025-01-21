24fdc18239ca4743c1d95a65750a23d7f375c291
Ricardo Camilo <ricardo_hks@hotmail.com>
2025-01-21 11:22:55 -0300

    Refactor DesktopMenu component for improved localization
    - Removed the `menuItems` prop and integrated the `menuItems` from static imports to streamline the component.
    - Replaced static link labels with dynamic translations using `t(menuItem.menuItemKey)` for enhanced localization support.
    - Updated `aria-label` attributes to utilize translated values, improving accessibility for screen readers.
    - These changes enhance the user experience by providing a more consistent and localized navigation menu across different languages.

--- src/components/DesktopMenu.tsx
+++ src/components/DesktopMenu.tsx
@@ -1,23 +1,25 @@
 import Link from "next/link";
-import { MenuItem } from "@/types/menuItem";
 import { getUniqueId } from "@/utils/getUniqueId";
+import { menuItems } from "@/static/menuItems";
+import { useTranslation } from "@/hooks/useTranslation";
 
 interface DesktopMenuProps {
-  menuItems: MenuItem[];
   className?: string;
 }
 
-export function DesktopMenu({ menuItems, className }: DesktopMenuProps) {
+export function DesktopMenu({ className }: DesktopMenuProps) {
+  const { t } = useTranslation();
+
   return (
     <ul className={`hidden lg:flex items-center space-x-8 ${className}`}>
-      {menuItems.map((item, index) => (
-        <li key={`${item.label}-${index}-desktop-menu-${getUniqueId()}`}>
+      {menuItems.map((menuItem, index) => (
+        <li key={`${menuItem.menuItemKey}-${index}-desktop-menu-${getUniqueId()}`}>
           <Link
-            href={item.to}
-            aria-label={item.ariaLabel}
+            href={menuItem.to}
+            aria-label={menuItem.ariaLabel}
             className="text-white hover:text-white px-3 py-2 rounded-md text-sm font-medium transition-colors duration-200"
           >
-            {item.label}
+            {t(menuItem.menuItemKey)}
           </Link>
         </li>
       ))}
