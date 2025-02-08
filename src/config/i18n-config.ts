373e2bc07514f50e7c4c17d89f9c6818b779a2af
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 10:58:58 -0300

    feat(localization): Add Japanese locale to supported languages

--- src/config/i18n-config.ts
+++ src/config/i18n-config.ts
@@ -1,4 +1,4 @@
-export const locales = ['en', 'pt', 'hi'] as const;
+export const locales = ['en', 'pt', 'hi', 'jp'] as const;
 export const defaultLocale = 'pt' as const;
 
 export type Locale = (typeof locales)[number];
