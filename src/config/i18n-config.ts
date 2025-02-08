19d17271f0095562a0e740a2bcc574b9fdd7cca4
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 11:29:47 -0300

    feat(localization): Update supported locales with Arabic, German, and Polish

--- src/config/i18n-config.ts
+++ src/config/i18n-config.ts
@@ -1,4 +1,4 @@
-export const locales = ['en', 'pt', 'hi', 'jp'] as const;
+export const locales = ['en', 'pt', 'hi', 'jp', 'de', 'ar', 'pl'] as const;
 export const defaultLocale = 'pt' as const;
 
 export type Locale = (typeof locales)[number];
