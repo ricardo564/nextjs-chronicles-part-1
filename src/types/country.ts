03158f4b71ede9380ac7a83c65abed139a2a7cd6
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 02:42:32 -0300

    feat(types): Refactor Country interface and add CountryResponse interface

--- src/types/country.ts
+++ src/types/country.ts
@@ -6,3 +6,27 @@ export interface Country {
   region?: string
   population?: number
 }
+
+export interface CountryResponse {
+  name: {
+    common: string;
+    official: string;
+
+  };
+  cca2: string;
+  flags: {
+    png: string;
+    svg: string;
+  };
+  idd: {
+    root: string;
+    suffixes: string[];
+  };
+  capital?: string[];
+  languages?: Record<string, string>;
+  currencies?: Record<string, {
+    name: string;
+    symbol: string;
+  }>;
+}
+
