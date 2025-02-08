5126cfa87506fb59ee6b822469f021a4d658b9df
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 20:47:41 -0300

    feat(forms): Enhance TextInput component with additional props and styling

--- src/components/forms/TextInput.tsx
+++ src/components/forms/TextInput.tsx
@@ -1,4 +1,5 @@
-import { FC } from "react";
+import type { FC, FocusEvent } from "react";
+import {ReactNode} from "react";
 import {
   FieldValues,
   Path,
@@ -9,29 +10,33 @@ import Label from "@/components/Label";
 
 interface Props {
   name: string;
+  type?: "text" | "email" | "password" | "date" | "checkbox";
   label?: string;
   className?: string;
   placeholder?: string;
   disabled?: boolean;
   error?: string;
+  children?: ReactNode;
   register: UseFormRegister<FieldValues>;
   rules?: RegisterOptions<FieldValues, Path<FieldValues>>;
-  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
 }
 
 export const TextInput: FC<Props> = ({
   name,
+  type = "text",
   label,
   className,
   placeholder,
   disabled = false,
   error,
+  children,
   register,
   rules,
   onBlur,
 }: Props) => {
   return (
-    <div className={`flex flex-col ${className}`}>
+    <div className={`flex flex-col relative ${className}`}>
       {label && (
         <Label
           className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent  rounded-full"
@@ -41,13 +46,18 @@ export const TextInput: FC<Props> = ({
       )}
       <input
         {...register(name, rules)}
-        type="text"
+        type={type}
         id={name}
         placeholder={placeholder}
         disabled={disabled}
         className="z-[2] w-full px-4 py-3 rounded-lg bg-gray-700 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition disabled:bg-gray-600 disabled:border-gray-400 disabled:text-gray-300 disabled:placeholder-gray-200 disabled:cursor-not-allowed"
         onBlur={onBlur}
       />
+      {children && (
+        <div className="absolute inset-y-0 right-0 flex items-center pr-3 z-[3] justify-center h-12 mt-6">
+          {children}
+        </div>
+      )}
       {error && <p className="mt-1 text-sm text-red-400">{error}</p>}
     </div>
   );
