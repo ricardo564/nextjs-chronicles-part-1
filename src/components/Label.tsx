4793f357b09ae687573362c6df0cd4c148305002
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 20:48:17 -0300

    feat(forms): Update Label component to support additional children and improve layout

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,16 +1,22 @@
 import type { FC } from "react";
+import { ReactNode } from "react";
 
 interface Props {
   value: string;
   className?: string;
   htmlFor?: string;
+  children?: ReactNode;
 }
 
-const Label: FC<Props> = ({ value, className, htmlFor }: Props) => {
+const Label: FC<Props> = ({ value, className, htmlFor, children }: Props) => {
   return (
     <div className={className}>
-      <label className="animation-fade-in-from-left transition-all duration-300" htmlFor={htmlFor}>
-        <span className="text-[1rem] font-medium">{value}</span>
+      <label
+        className="aimation-fade-in-from-left transition-all duration-300 flex flex-row items-center justify-center"
+        htmlFor={htmlFor}
+      >
+        <span className="text-[1rem] font-medium w-full">{value}</span>
+        {children}
       </label>
     </div>
   );
