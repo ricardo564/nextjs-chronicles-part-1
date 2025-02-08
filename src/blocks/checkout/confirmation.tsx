18c5c66d8d29a84c8e4ffe027168de71678b7bba
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-08 11:02:26 -0300

    feat(checkout): Add finalize order button to ConfirmationStep

--- src/blocks/checkout/confirmation.tsx
+++ src/blocks/checkout/confirmation.tsx
@@ -1,7 +1,15 @@
-export const ConfirmationStep = () => {
+import Button from "@/components/Button";
+import Link from "@/components/Link";
+
+const ConfirmationStep = () => {
   return (
-    <div>
+    <div className="min-h-screen">
       <h1>Confirmation</h1>
+      <Link href="/orders">
+        <Button label="Finalize Order" />
+      </Link>
     </div>
   );
 };
+
+export default ConfirmationStep
