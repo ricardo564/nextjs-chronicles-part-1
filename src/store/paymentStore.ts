0ce2231ab29b133c99b5663e7775faa9b713fd49
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-09 16:31:01 -0300

    feat(store): Create payment store with Zustand for managing payment information

---
+++ src/store/paymentStore.ts
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { PaymentInformation } from "@/types/payment";
+
+interface PaymentStore {
+  paymentInfo: PaymentInformation;
+  setPaymentInfo: (info: PaymentInformation) => void;
+  clearPaymentInfo: () => void;
+}
+
+const paymentInfoDefaultValue: PaymentInformation = {
+  paymentMethod: 'bitcoin',
+  creditCard: {
+    number: '',
+    name: '',
+    expirationDate: '',
+    cvv: ''
+  },
+  boleto: {
+    number: ''
+  }
+}
+
+export const usePaymentStore = create<PaymentStore>()(
+  persist(
+    (set) => ({
+      paymentInfo: {
+        paymentMethod: 'bitcoin',
+        creditCard: {
+          number: '',
+          name: '',
+          expirationDate: '',
+          cvv: ''
+        },
+        boleto: {
+          number: ''
+        }
+      },
+      setPaymentInfo: (info) => set({ paymentInfo: info }),
+      clearPaymentInfo: () => set({ paymentInfo: paymentInfoDefaultValue }),
+    }),
+    {
+      name: 'payment-store',
+    }
+  )
+);
