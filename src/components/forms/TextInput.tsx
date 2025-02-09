3d40c9a60dbf5228b0a332e4fbbcc3271e47deec
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-09 01:45:33 -0300

    feat(ui): Enhance TextInput component with generic type support

--- src/components/forms/TextInput.tsx
+++ src/components/forms/TextInput.tsx
@@ -1,5 +1,5 @@
 import type { FC, FocusEvent } from "react";
-import {ReactNode} from "react";
+import { ReactNode } from "react";
 import {
   FieldValues,
   Path,
@@ -8,8 +8,8 @@ import {
 } from "react-hook-form";
 import Label from "@/components/Label";
 
-interface Props {
-  name: string;
+interface TextInputProps<T extends FieldValues> {
+  name: Path<T>;
   type?: "text" | "email" | "password" | "date" | "checkbox";
   label?: string;
   className?: string;
@@ -17,12 +17,12 @@ interface Props {
   disabled?: boolean;
   error?: string;
   children?: ReactNode;
-  register: UseFormRegister<FieldValues>;
-  rules?: RegisterOptions<FieldValues, Path<FieldValues>>;
+  register: UseFormRegister<T>;
+  rules?: RegisterOptions<T, Path<T>>;
   onBlur?: (e: FocusEvent<HTMLInputElement>) => void;
 }
 
-export const TextInput: FC<Props> = ({
+export const TextInput: FC<TextInputProps<FieldValues>> = ({
   name,
   type = "text",
   label,
@@ -34,20 +34,20 @@ export const TextInput: FC<Props> = ({
   register,
   rules,
   onBlur,
-}: Props) => {
+}: TextInputProps<FieldValues>) => {
   return (
     <div className={`flex flex-col relative ${className}`}>
       {label && (
         <Label
           className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent  rounded-full"
           value={label}
-          htmlFor={name}
+          htmlFor={name.toString()}
         />
       )}
       <input
         {...register(name, rules)}
         type={type}
-        id={name}
+        id={name.toString()}
         placeholder={placeholder}
         disabled={disabled}
         className="z-[2] w-full px-4 py-3 rounded-lg bg-gray-700 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition disabled:bg-gray-600 disabled:border-gray-400 disabled:text-gray-300 disabled:placeholder-gray-200 disabled:cursor-not-allowed"
