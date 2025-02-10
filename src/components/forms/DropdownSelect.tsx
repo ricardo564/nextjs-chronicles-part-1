1ef599ea0770393943aca2b75a3f69fe22ece7ad
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-09 21:14:28 -0300

    refactor(forms): Simplify DropdownSelect component with native select element

--- src/components/forms/DropdownSelect.tsx
+++ src/components/forms/DropdownSelect.tsx
@@ -1,5 +1,4 @@
-import type { FC, FocusEvent } from "react";
-import { useState, useEffect, ChangeEvent } from "react";
+import { FC } from "react";
 import {
   FieldValues,
   Path,
@@ -7,164 +6,76 @@ import {
   UseFormRegister,
 } from "react-hook-form";
 import Label from "@/components/Label";
-import AnimatedChevron from "@/components/AnimatedChevron";
 
 interface Option {
+  value: string;
   label: string;
-  value: string | number;
 }
 
-interface DropdownSelectProps {
+interface Props {
   name: string;
-  options: Option[];
   label?: string;
-  placeholder?: string;
-  initialValue?: string | number;
+  options: Option[];
   className?: string;
+  defaultValue?: string;
+  disabled?: boolean;
+  error?: string;
   register: UseFormRegister<FieldValues>;
   rules?: RegisterOptions<FieldValues, Path<FieldValues>>;
-  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
   onChange?: (value: string | number) => void;
 }
 
-const DropdownSelect: FC<DropdownSelectProps> = ({
+const DropdownSelect: FC<Props> = ({
   name,
   label,
-  placeholder,
-  initialValue,
-  className,
   options,
-  rules,
+  className,
+  defaultValue,
+  disabled = false,
+  error,
   register,
-  onBlur,
+  rules,
   onChange,
 }) => {
-  const [isDropdownOpen, setDropdownOpen] = useState(false);
-  const [selectedLabel, setSelectedLabel] = useState("");
-  const [filteredOptions, setFilteredOptions] = useState(options);
-  const [searchQuery, setSearchQuery] = useState("");
+  const { onChange: registerOnChange, ...rest } = register(name, rules);
 
-  const toggleDropdown = () => {
-    if (isDropdownOpen) {
-      setSearchQuery("");
-      setFilteredOptions(options);
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    registerOnChange(e);
+
+    if (onChange) {
+      onChange(e.target.value);
     }
-    setDropdownOpen(!isDropdownOpen);
   };
-
-  const handleSelect = (value: string | number) => {
-    const selectedOption = options.find((option) => option.value === value);
-    if (selectedOption) {
-      setSelectedLabel(selectedOption.label);
-      if (onBlur) {
-        const event = new FocusEvent('blur') as unknown as FocusEvent<HTMLInputElement>;
-        onBlur(event);
-      }
-
-      if (onChange) {
-        onChange(value);
-      }
-    }
-    setDropdownOpen(false);
-    setSearchQuery("");
-  };
-
-  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = event.target.value;
-    setSearchQuery(searchTerm);
-    setFilteredOptions(
-      options.filter((option) =>
-        option.label.toLowerCase().includes(searchTerm.toLowerCase())
-      )
-    );
-  };
-
-  useEffect(() => {
-    if (initialValue !== undefined) {
-      const initialOption = options.find(
-        (option) => option.value === initialValue
-      );
-      if (initialOption) {
-        setSelectedLabel(initialOption.label);
-      }
-    } else if (options.length > 0) {
-      setSelectedLabel(options[0].label);
-    }
-  }, [initialValue, options]);
-
-  if (options.length === 0) {
-    return (
-      <div className="flex flex-col w-full relative">
-        <Label
-          className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent rounded-full"
-          value={"Error loading options for " + label}
-        />
-        <div className="w-full h-[3.1rem] bg-gray-700 border border-gray-600 rounded-lg z-[2] flex items-center justify-between transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent disabled:opacity-50 disabled:cursor-not-allowed"></div>
-      </div>
-    )
-  }
 
   return (
-    <div className={`flex flex-col w-full relative ${className}`}>
+    <div className={`flex flex-col ${className}`}>
       {label && (
         <Label
           className="ml-1 -mb-2 text-white w-max p-2 text-xs font-normal bg-gradient-to-r from-white/10 via-transparent rounded-full"
           value={label}
+          htmlFor={name}
         />
       )}
-      <button
-        type="button"
-        className="z-[2] w-full flex items-center justify-between bg-gray-700 border border-gray-600 rounded-lg transition-all duration-300 hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent disabled:opacity-50 disabled:cursor-not-allowed"
-        aria-haspopup="listbox"
-        aria-expanded={isDropdownOpen}
-        aria-controls={`${name}-dropdown`}
-        onClick={toggleDropdown}
-      >
-        <input
-          {...register(name, rules)}
-          className="w-full px-4 py-3 bg-transparent text-white placeholder-gray-400 focus:outline-none"
-          value={isDropdownOpen ? searchQuery : selectedLabel}
-          onClick={() => {
-            if (isDropdownOpen && selectedLabel) {
-              setSearchQuery(selectedLabel);
-              setSelectedLabel("");
-            }
-          }}
-          onChange={handleSearch}
-          placeholder={placeholder}
-          readOnly={!isDropdownOpen}
-        />
-        <div className="px-2">
-          <AnimatedChevron
-            className="w-8 h-8 text-white"
-            isOpen={isDropdownOpen}
-            onToggle={toggleDropdown}
-          />
-        </div>
-      </button>
 
-      <div
-        role="listbox"
-        id={`${name}-dropdown`}
-        className={`absolute top-full left-0 w-full mt-1 bg-gray-700 border border-gray-600 rounded-lg shadow-lg max-h-[20rem] overflow-hidden overflow-y-auto z-10 transition-all duration-200 ease-in-out animate-dropdown ${!isDropdownOpen ? '-translate-y-100 opacity-0 z-[-10]' : 'translate-y-2 opacity-100 z-[10]'}`}
+      <select
+        {...rest}
+        id={name}
+        onChange={handleChange}
+        disabled={disabled}
+        defaultValue={defaultValue}
+        className="w-full px-4 py-3 rounded-lg bg-gray-700 border border-gray-600 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-green-500 focus:border-transparent transition disabled:bg-gray-600 disabled:border-gray-400 disabled:text-gray-300 disabled:cursor-not-allowed"
       >
-        <ul className="list-none py-2">
-          {filteredOptions.map(({ label, value }, index) => (
-            <li key={`${name}-${label}-${value}-${index}`}>
-              <button
-                type="button"
-                role="option"
-                aria-selected={label === selectedLabel}
-                tabIndex={isDropdownOpen ? 0 : -1}
-                className="w-full px-4 py-2 text-left text-white hover:bg-gray-600 focus:outline-none focus:bg-gray-600 transition-colors"
-                onClick={() => handleSelect(value)}
-              >
-                {label}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <option value="" disabled>
+          Select an option
+        </option>
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+
+      {error && <p className="mt-1 text-sm text-red-400">{error}</p>}
     </div>
   );
 };
