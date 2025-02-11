3142d7f9625954ccb58637b0de0b01aa3cc4f72f
Ricardo Camilo | Programador | React & Vue.js | Frontend <39412058+ricardo-camilo-programador-frontend-web@users.noreply.github.com>
2025-02-11 14:28:52 -0300

    Add RandomBackground component to Orders page

--- src/app/[locale]/orders/page.tsx
+++ src/app/[locale]/orders/page.tsx
@@ -1,10 +1,13 @@
 import type { FC } from "react";
 import DefaultLayout from "@/layouts/DefaultLayout";
 import ComingSoon from "@/components/ComingSoon";
+import RandomBackground from "@/blocks/randomBackground";
 
 const OrdersPage: FC = () => {
   return (
     <DefaultLayout>
+      <RandomBackground />
+
       <div className="min-h-screen flex items-center justify-center">
         <ComingSoon />
       </div>
